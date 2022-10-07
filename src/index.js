2ae8f2254c422f8ca42233561dfb894751ff0891
ArthurKorchak <korchak.arthur@gmail.com>
2022-10-07 20:34:58 +0300

    Compleated functions

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import App from 'App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <BrowserRouter basename="/KidsLike">
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
---
+++ src/pages/ContactPage/ContactPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ContactList from 'modules/ContactList';
+// import s from './ContactPage.module.css';
+
+const ContactPage = () => {
+  return (
+    <>
+      <h1>Our team</h1>
+      <h2>Always ready for new challenges!</h2>
+      <ContactList />
+    </>
+  );
+};
+
+export default ContactPage;

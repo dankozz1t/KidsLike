0af865db3169a6dc9ef264efeec032775f3c72d9
Olena Yatseniuk <elena.yatsenyuk@gmail.com>
2022-10-07 22:28:55 +0300

    raw weektabs component

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,9 +1,15 @@
+import WeekTabs from 'modules/WeekTabs';
 import React from 'react';
 
 import s from './MainPage.module.css';
 
 const MainPage = () => {
-  return <h1 className={s.title}>MainPage</h1>;
+  return (
+    <>
+      <h1 className={s.title}>MainPage</h1>
+      <WeekTabs />
+    </>
+  );
 };
 
 export default MainPage;

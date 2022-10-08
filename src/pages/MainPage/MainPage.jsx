88ffe5386d914725feaec5ac011650f24a086b53
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-10-08 12:26:14 +0300

    rename path

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import s from './MainPage.module.css';
+import s from './MainPage.module.scss';
 
 const MainPage = () => {
   return <h1 className={s.title}>MainPage</h1>;

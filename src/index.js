a5a22c5177bb8d04f460d2c1451f95431c5ded44
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-10-08 10:42:42 +0300

    add scss and fonts

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from 'App';
 
-import './index.css';
+import './shared/styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

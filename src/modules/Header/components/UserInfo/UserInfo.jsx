aa4541804beccb636885463ffffc871545981d63
Mike Bakuzov <bakuzov@gmail.com>
2022-10-11 10:59:37 +0300

    clean code footer/header

--- src/modules/Header/components/UserInfo/UserInfo.jsx
+++ src/modules/Header/components/UserInfo/UserInfo.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import s from './UserInfo.module.scss';
 
-const UserInfo = ({ email }) => {
-  return (
-    <>
-      <p className={s.emailtext}>{email}</p>
-    </>
-  );
-};
+const UserInfo = ({ email }) => <p className={s.emailtext}>{email}</p>;
 
 export default UserInfo;

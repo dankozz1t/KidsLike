9c5f18d53019f3ee54962d4028d9d6a29574db10
Shpuntoff <v.shpuntoff@gmail.com>
2022-10-11 18:16:27 +0300

    Add Style to CardTitle

--- src/shared/components/CardTitle/CardTitle.js
+++ src/shared/components/CardTitle/CardTitle.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './CardTitle.module.css';
 
 const CardTitle = ({ task }) => (
   <div>
-    <span>{task.title}</span>
+    <span className={s.card_title}>{task.title}</span>
   </div>
 );
 

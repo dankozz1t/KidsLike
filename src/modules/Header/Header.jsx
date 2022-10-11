e253ab10211e3d3c42cdc5458898cc96a469435a
Mike Bakuzov <bakuzov@gmail.com>
2022-10-11 11:11:25 +0300

    deleted coment

--- src/modules/Header/Header.jsx
+++ src/modules/Header/Header.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import Logo from 'shared/components/Logo';
-// import Footer from 'shared/components/Footer';
 import { useSelector } from 'react-redux';
 import { getLoggedIn } from 'redux/auth/auth.selector';
-// import Navigation from '';
 import UserMenu from './components/UserMenu/UserMenu';
 import AuthNav from './components/AuthNav/AuthNav';
 

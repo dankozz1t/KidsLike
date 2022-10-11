733aa17645a723c85c88e16fd891add100ec2011
Shpuntoff <v.shpuntoff@gmail.com>
2022-10-11 14:57:12 +0300

    CardList Styles

--- src/shared/components/CardsList/CardsList.js
+++ src/shared/components/CardsList/CardsList.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Card from '../Card';
+import s from './CardsList.module.scss';
 
 const CardsList = ({ tasks }) => {
   return (
-    <ul>
+    <ul className={s.cards_list}>
       {tasks.map(task => {
         const { _id: id } = task;
         return (
-          <li key={id}>
+          <li key={id} className={s.card_item}>
             <Card {...task} />
           </li>
         );

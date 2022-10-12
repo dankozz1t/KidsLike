3f032e48adcc585ac87ba8e6f8a2769a00ea07f2
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-10-12 21:58:31 +0300

    fix toggle

--- src/shared/components/CardFooter/CardFooter.jsx
+++ src/shared/components/CardFooter/CardFooter.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useSearchParams } from 'react-router-dom';
 import CardTitle from '../CardTitle';
 import PointAmount from '../PointAmount';
 import TaskToggle from '../TaskToggle';
 
+import { ReactComponent as IconSuccess } from 'assets/images/icon/icon-success.svg';
+import { ReactComponent as IconDanger } from 'assets/images/icon/icon-danger.svg';
+
 import s from './CardFooter.module.scss';
 
 const CardFooter = ({ ...taskInfo }) => {
-  const { title, reward } = taskInfo;
+  const { _id, title, reward, isCompleted } = taskInfo;
   const { pathname } = useLocation();
+
+  const currentWeekDay = new Date().toLocaleString('en-US', {
+    weekday: 'long',
+  });
+  const [searchParams] = useSearchParams();
 
   const renderElement = () => {
     if (pathname === '/main') {
-      return <TaskToggle />;
+      if (currentWeekDay === searchParams.get('day')) {
+        return <TaskToggle _id={_id} isCompleted={isCompleted} />;
+      }
+
+      return isCompleted ? <IconSuccess /> : <IconDanger />;
     }
   };
 
--- src/shared/service/task.service.js
+++ src/shared/service/task.service.js
@@ -8,6 +8,6 @@ export const addTaskToProvidedDaysService = async body => {
   return await privateApi.patch('/task/single-active', body);
 };
 
-export const toggleTaskStatusService = async body => {
-  return await privateApi.patch('/task/switch', body);
+export const toggleTaskStatusService = async (id, body) => {
+  return await privateApi.patch(`/task/switch/${id}`, body);
 };

49876bf2f5f751c549d4506020e9443da8763b25
Shpuntoff <v.shpuntoff@gmail.com>
2022-10-12 15:39:46 +0300

    fix card element

--- src/shared/components/CardBody/CardBody.jsx
+++ src/shared/components/CardBody/CardBody.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './CardBody.module.scss'
 
 const CardBody = ({ img, alt}) => (
   <img 
-  className="card__image" 
+  className={s.card__image} 
   src={img}
   alt={alt} />
 );

1359b6fd95fbb4e647802cb578ee1f23368266d3
ANDRII HALYTSKYI <galickyav@gmail.com>
2022-10-12 20:11:09 +0300

    Modal-Uload
    Ошибка запроса "не авторизовано"

--- src/shared/components/ModalContentCreateTask/ModalContentCreateTask.jsx
+++ src/shared/components/ModalContentCreateTask/ModalContentCreateTask.jsx
@@ -3,10 +3,18 @@ import modalRobot from 'assets/images/modal-image/modal-robot.png';
 import modalImage from 'assets/images/modal-image/modal-image.svg';
 import modalEditInput from 'assets/images/modal-image/edit-24px 2.svg';
 import s from './ModalContentCreateTask.module.scss';
+import { useRef } from "react";
+import { privateApi, token } from 'shared/service/http/http';
+
 
 const ModalContentCreateTask = () => {
+
+
+    const imageInputRef = useRef(null)
+
     const [taskName, setTaskName] = useState("");
     const [reward, setReward] = useState("");
+    const [image, setImage] = useState(null);
 
     const onHandleChangeTaskName = (e) => {
         setTaskName(e.target.value);
@@ -16,8 +24,29 @@ const ModalContentCreateTask = () => {
         setReward(e.target.value);
     };
 
+    const onHandleChangeImage = (e) => {
+        setImage(e.target.files[0]);
+    };
+
     const onHandleSubmit = (e) => {
         e.preventDefault();
+
+        if (image.size > 204800) {
+            return
+        }
+
+        const body = new FormData()
+
+        body.append('title', taskName)
+        body.append('reward', reward)
+        body.append('file', image)
+
+        privateApi.post('/task', body, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+                'Content-Type': 'multipart/form-data',
+            }
+        })
         setTaskName('');
         setReward('');
     };
@@ -25,9 +54,22 @@ const ModalContentCreateTask = () => {
     return (
         <div className={s.container}>
             {<img className={s.modalRobot} src={modalRobot} alt="modalcat" />}
-            {<img className={s.modalImage} src={modalImage} alt="modalImage" />}
+            <button
+                type="button"
+                className={s.addImageBtn}
+                onClick={() => imageInputRef.current?.click()}>
+                <img className={s.modalImage} src={modalImage} alt="" />
+            </button>
+            {/* {<img className={s.modalImage} src={modalImage} alt="modalImage" />} */}
 
             <form className={s.modalForm} onSubmit={onHandleSubmit}>
+                <input
+                    type="file"
+                    hidden
+                    ref={imageInputRef}
+                    onChange={onHandleChangeImage} />
+                
+
                 <div className={s.addTask}>
                     <label className={s.modalInputLabel}>
                         <img className={s.modalEditInput} src={modalEditInput} alt="modalEditInput" />

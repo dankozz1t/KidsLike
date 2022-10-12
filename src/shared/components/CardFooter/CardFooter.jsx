9b9c88155ca556cc077cccb53b498faf3c2d1d76
Shpuntoff <v.shpuntoff@gmail.com>
2022-10-12 19:25:21 +0300

    Create Toggle

--- src/shared/components/CardFooter/CardFooter.jsx
+++ src/shared/components/CardFooter/CardFooter.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { useLocation } from "react-router-dom";
 import CardTitle from '../CardTitle';
 import PointAmount from '../PointAmount';
+import TaskToggle from '../TaskToggle';
 import s from './CardFooter.module.scss'
 
 const CardFooter = ({ ...taskInfo }) => {
     const { title, reward } = taskInfo;
+    const { pathname } = useLocation();
+
+    const renderElement = () => {
+      if (pathname === '/main') {
+        return (
+          <TaskToggle/>
+        );
+      }
+
+    }
 
     return (
       <div className={s.card_footer}>
@@ -12,6 +24,7 @@ const CardFooter = ({ ...taskInfo }) => {
           <CardTitle title={title} />
           <PointAmount point={reward} />
         </div>
+        <>{renderElement()}</>
       </div>
     );
   };
---
+++ src/shared/components/TaskToggle/TaskToggle.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import s from './TaskToggle.module.scss';
+import { ReactComponent as CheckSymbol } from './images/check-symbol.svg';
+import { ReactComponent as Attention } from './images/attention.svg';
+
+const TaskToggle = ({ id }) => {
+
+  return (
+    <label htmlFor={id} className={s.switch}>
+      <input
+        id={id}
+        className={s.checkbox}
+        type="checkbox"
+      />
+      <span className={`${s.slider} ${s.round}`}>
+        <CheckSymbol className={s.check_symbol} width="10" height="10" />
+        <Attention className={s.attention} width="5" height="12" />
+      </span>
+    </label>
+  );
+};
+export default TaskToggle;

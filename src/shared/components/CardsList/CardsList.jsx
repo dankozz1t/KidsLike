11c1d10409a910d0825f8d8c0d48cfd4cb39b57e
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-10-12 19:01:50 +0300

    add tasks

--- src/shared/components/CardsList/CardsList.jsx
+++ src/shared/components/CardsList/CardsList.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import { useLocation } from "react-router-dom";
 import Card from '../Card';
 import s from './CardsList.module.scss';
 
 const CardsList = ({ tasks }) => {
-  const location = useLocation().pathname;
-  console.log(location)
   return (
     <ul className={s.cards_list}>
       {tasks.map(task => {

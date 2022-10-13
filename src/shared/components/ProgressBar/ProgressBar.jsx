a2a7850c0fcc4f4b9f05ab803bf137476d30a6e5
Myrhoiazov <denis.mirgoyazov@gmail.com>
2022-10-13 10:16:42 +0300

    progressbar - done

--- src/shared/components/ProgressBar/ProgressBar.jsx
+++ src/shared/components/ProgressBar/ProgressBar.jsx
@@ -1,25 +1,40 @@
 import s from './ProgressBar.module.scss';
 import { Progress } from 'react-sweet-progress';
 import 'react-sweet-progress/lib/style.css';
+// import { useState } from 'react';
+import { useSelector } from 'react-redux';
 
 const ProgressBar = () => {
+  const points = useSelector(state => state.task.rewardsGained) + 6;
+  const plannedPoints = useSelector(state => state.task.rewardsPlanned) + 10;
+
+  let percent;
+
+  if (plannedPoints) {
+    percent = parseInt((points / plannedPoints) * 100);
+  }
+
+  if (points === 0) {
+    percent = 0;
+  }
+
   return (
     <div className={s.wrapper}>
       <p className={s.pointsInfo}>
         Points earned this week:
-        <span className={s.points}>0</span>
+        <span className={s.points}>{points}</span>
       </p>
       <p className={s.pointsInfo}>
         Planned points for this week:
-        <span className={s.points}>0</span>
+        <span className={s.points}>{plannedPoints}</span>
       </p>
       <div className={s.progressWrapper}>
         <span className={s.pointsAmount}>
-          <span className={s.collectedPoints}>0</span> / 0
+          <span className={s.collectedPoints}>{points}</span> / {plannedPoints}
         </span>
         <div className={s.progressTool}>
           <Progress
-            percent={30}
+            percent={percent >= 100 ? 100 : percent}
             theme={{
               success: {
                 symbol: '🏄‍',

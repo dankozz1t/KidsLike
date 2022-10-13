24bdba19c2785286ca77ba402b5bce77e0199299
ANDRII HALYTSKYI <105043191+Zorotool@users.noreply.github.com>
2022-10-13 13:08:09 +0300

    Andrii/Modal

--- src/shared/components/ModalContentCreateTask/ModalContentCreateTask.jsx
+++ src/shared/components/ModalContentCreateTask/ModalContentCreateTask.jsx
@@ -3,10 +3,19 @@ import modalRobot from 'assets/images/modal-image/modal-robot.png';
 import modalImage from 'assets/images/modal-image/modal-image.svg';
 import modalEditInput from 'assets/images/modal-image/edit-24px 2.svg';
 import s from './ModalContentCreateTask.module.scss';
+import { useRef } from "react";
+import { useDispatch } from "react-redux";
+import { createTaskThunk } from "redux/task/task.thunk";
+
 
 const ModalContentCreateTask = () => {
+
+const dispatch = useDispatch()
+    const imageInputRef = useRef(null)
+
     const [taskName, setTaskName] = useState("");
     const [reward, setReward] = useState("");
+    const [image, setImage] = useState(null);
 
     const onHandleChangeTaskName = (e) => {
         setTaskName(e.target.value);
@@ -16,8 +25,28 @@ const ModalContentCreateTask = () => {
         setReward(e.target.value);
     };
 
+    const onHandleChangeImage = (e) => {
+        setImage(e.target.files[0]);
+    };
+
     const onHandleSubmit = (e) => {
         e.preventDefault();
+        
+        
+        // if (image.size > 204800) {
+        //     return
+        // }
+        
+        const body = new FormData()
+        
+        body.append('title', taskName)
+        body.append('reward', reward)
+        body.append('file', image)
+        console.log(taskName);
+        console.log(reward);
+        console.log(image);
+        dispatch(createTaskThunk(body));
+        console.log(body);
         setTaskName('');
         setReward('');
     };
@@ -25,9 +54,22 @@ const ModalContentCreateTask = () => {
     return (
         <div className={s.container}>
             {<img className={s.modalRobot} src={modalRobot} alt="modalcat" />}
-            {<img className={s.modalImage} src={modalImage} alt="modalImage" />}
+            <button
+                type="button"
+                className={s.addImageBtn}
+                onClick={() => imageInputRef.current?.click()}>
+                <img className={s.modalImage} src={modalImage} alt="" />
+            </button>
+            {/* {<img className={s.modalImage} src={modalImage} alt="modalImage" />} */}
 
             <form className={s.modalForm} onSubmit={onHandleSubmit}>
+                <input
+                    type="file"
+                    hidden
+                    ref={imageInputRef}
+                    onChange={onHandleChangeImage} />
+                
+
                 <div className={s.addTask}>
                     <label className={s.modalInputLabel}>
                         <img className={s.modalEditInput} src={modalEditInput} alt="modalEditInput" />

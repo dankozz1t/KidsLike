b852ba40a567da8ebafb75adf2eb245358db39ed
Myrhoiazov <denis.mirgoyazov@gmail.com>
2022-10-13 09:31:17 +0300

    add progrssBar

--- src/shared/components/ProgressBar/ProgressBar.jsx
+++ src/shared/components/ProgressBar/ProgressBar.jsx
@@ -1,4 +1,6 @@
 import s from './ProgressBar.module.scss';
+import { Progress } from 'react-sweet-progress';
+import 'react-sweet-progress/lib/style.css';
 
 const ProgressBar = () => {
   return (
@@ -11,12 +13,30 @@ const ProgressBar = () => {
         Planned points for this week:
         <span className={s.points}>0</span>
       </p>
-      <p className={s.progressWrapper}>
+      <div className={s.progressWrapper}>
         <span className={s.pointsAmount}>
-          <span className={s.collectedPoints}>{0}</span> / {0}
+          <span className={s.collectedPoints}>0</span> / 0
         </span>
-        <span className={s.progressTool}></span>
-      </p>
+        <div className={s.progressTool}>
+          <Progress
+            percent={30}
+            theme={{
+              success: {
+                symbol: '🏄‍',
+                color: 'rgb(223, 105, 180)',
+              },
+              active: {
+                symbol: '😀',
+                color: '#fbc630',
+              },
+              default: {
+                symbol: '🤔',
+                color: '#fbc630',
+              },
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 };

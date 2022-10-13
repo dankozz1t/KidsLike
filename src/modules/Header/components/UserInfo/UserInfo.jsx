88f1a9d08b83e70187ffa31de2f62e731197abc1
Mike Bakuzov <bakuzov@gmail.com>
2022-10-13 13:16:35 +0300

    add sign

--- src/modules/Header/components/UserInfo/UserInfo.jsx
+++ src/modules/Header/components/UserInfo/UserInfo.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import s from './UserInfo.module.scss';
 
-const UserInfo = ({ email }) => <p className={s.emailtext}>{email}</p>;
+const UserInfo = ({ email }) => {
+  let firstChar = email[0].toUpperCase();
+  return (
+    <>
+      <div className={s.char}>{firstChar} </div>
+      <p className={s.emailtext}>{email}</p>
+    </>
+  );
+};
 
 export default UserInfo;

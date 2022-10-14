774d34ae8953c4bba6dcf02502209ee698b3d161
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-10-14 11:01:22 +0300

    fix invalid data

--- src/modules/Header/components/UserInfo/UserInfo.jsx
+++ src/modules/Header/components/UserInfo/UserInfo.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import s from './UserInfo.module.scss';
 
 const UserInfo = ({ email }) => {
-  let firstChar = email[0].toUpperCase();
-  return (
-    <>
-      <div className={s.char}>{firstChar} </div>
-      <p className={s.emailtext}>{email}</p>
-    </>
-  );
+  if (email) {
+    const firstChar = email[0]?.toUpperCase();
+
+    return (
+      <>
+        <div className={s.char}>{firstChar} </div>
+        <p className={s.emailtext}>{email}</p>
+      </>
+    );
+  }
 };
 
 export default UserInfo;

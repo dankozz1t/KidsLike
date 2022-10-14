abe0ea0aaf5f16502be56405d7e28b14dcbfe6be
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-10-14 10:52:30 +0300

    fix header

---
+++ src/modules/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Logo from 'shared/components/Logo';
+import { useSelector } from 'react-redux';
+import { getLoggedIn } from 'redux/auth/auth.selector';
+import UserMenu from './components/UserMenu/UserMenu';
+import AuthNav from './components/AuthNav/AuthNav';
+
+import s from './Header.module.scss';
+
+const Header = () => {
+  const isLoggedIn = useSelector(getLoggedIn);
+  return (
+    <header className={s.header}>
+      <div className={s.wrapper}>
+        <Logo classFor="Header" />
+
+        {isLoggedIn ? <UserMenu /> : <AuthNav />}
+      </div>
+    </header>
+  );
+};
+
+export default Header;

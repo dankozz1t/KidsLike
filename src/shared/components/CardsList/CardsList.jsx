fee06054f4f7106521195e8412312fad47d3c10d
Shpuntoff <v.shpuntoff@gmail.com>
2022-10-14 14:18:23 +0300

    delete center

--- src/shared/components/CardsList/CardsList.jsx
+++ src/shared/components/CardsList/CardsList.jsx
@@ -1,24 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useLocation } from 'react-router-dom';
-import classNames from 'classnames';
 import Card from '../Card';
 import s from './CardsList.module.scss';
 
 const CardsList = ({ tasks }) => {
-  const { pathname } = useLocation();
-  let classes = classNames(`${s.cards_list} ${s.center}`);
-
-  if (pathname === '/main') {
-    classes = s.cards_list;
-  }
-
   return (
-    <ul className={classes}>
+    <ul className={s.cards_list}>
       {tasks.map(task => {
         let id;
         task.id ? (id = task.id) : (id = task._id);
-        // const { _id: id } = task;
+        
         return (
           <li key={id} className={s.card_item}>
             <Card {...task} />

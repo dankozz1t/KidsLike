d09fb1ee700bcc876030acd77b702b85f1cb41a3
Shpuntoff <v.shpuntoff@gmail.com>
2022-10-14 14:06:01 +0300

    Styled card and fix container

--- src/shared/components/CardsList/CardsList.jsx
+++ src/shared/components/CardsList/CardsList.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useLocation } from 'react-router-dom';
+import classNames from 'classnames';
 import Card from '../Card';
 import s from './CardsList.module.scss';
 
 const CardsList = ({ tasks }) => {
+  const { pathname } = useLocation();
+  let classes = classNames(`${s.cards_list} ${s.center}`);
+
+  if (pathname === '/main') {
+    classes = s.cards_list;
+  }
+
   return (
-    <ul className={s.cards_list}>
+    <ul className={classes}>
       {tasks.map(task => {
         let id;
         task.id ? (id = task.id) : (id = task._id);

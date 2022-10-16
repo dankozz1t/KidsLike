65bca06c8242d724fab5222469ff6f6b65fbf753
Myrhoiazov <denis.mirgoyazov@gmail.com>
2022-10-16 22:54:31 +0200

    add header nav

--- src/modules/Header/components/UserMenu/UserMenu.jsx
+++ src/modules/Header/components/UserMenu/UserMenu.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import s from './UserMenu.module.scss';
 import { ReactComponent as LogoAuth } from 'assets/images/icon/icon-auth.svg';
@@ -8,18 +8,30 @@ import { getUser } from 'redux/auth/auth.selector';
 import VerticalBar from 'shared/components/VerticalBar/VerticalBar';
 import Modal from 'shared/components/Modal';
 import ModalLogout from 'shared/components/ModalLogout';
-
+import { ReactComponent as IconBurgerMenu } from 'assets/images/icon/icon-burgerMenu.svg';
+import { ReactComponent as CloseBurgerMenu } from 'assets/images/icon/icon-BurgerClose.svg';
+import BurgerMenu from 'shared/components/BurgerMenu/BurgerMenu';
 
 const UserMenu = () => {
   const { email, balance } = useSelector(getUser);
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [isShowNav, setIsShowNav] = useState(false);
 
+  const handleIsShowMenu = () => {
+    setIsShowNav(!isShowNav);
+  };
 
   return (
     <div className={s.subcontainer}>
       <div className={s.boxbalance}>
         <p className={s.text}>Score balance:</p>
         <span className={s.balance}>{balance}</span>
+      </div>
+
+      <div className={s.burger_menu}>
+        <BurgerMenu onClick={handleIsShowMenu}>
+          <IconBurgerMenu />
+        </BurgerMenu>
       </div>
       <nav className={s.navigation}>
         <NavLink to="/main" className={s.link}>
@@ -44,14 +56,59 @@ const UserMenu = () => {
         <button
           className={s.btnlogout}
           type="button"
-          onClick={() => setOpen(true)}        >
-          
-          <LogoAuth />
+          onClick={() => setOpen(true)}
+        >
+          <LogoAuth className={s.logo__auth} />
         </button>
         <Modal open={open} onClose={() => setOpen(false)}>
           <ModalLogout onClose={() => setOpen(false)} />
         </Modal>
       </div>
+
+      {isShowNav && (
+        <div className={s.modal_nav}>
+          <div className={s.modal_nav_wrapper}>
+            <div className={s.burger_menu}>
+              <div className={s.box__mobile}>
+                <UserInfo
+                  email={email.slice(0, 5)}
+
+                />
+                <VerticalBar classFor="Header" />
+                <button
+                  className={s.btnlogout__modal}
+                  type="button"
+                  onClick={() => setOpen(true)}
+                >
+                  <LogoAuth className={s.icon__logout} />
+                </button>
+                <Modal open={open} onClose={() => setOpen(false)}>
+                  <ModalLogout onClose={() => setOpen(false)} />
+                </Modal>
+              </div>
+              <div className={s.burger_menu_close}>
+                <BurgerMenu onClick={handleIsShowMenu}>
+                  <CloseBurgerMenu />
+                </BurgerMenu>
+              </div>
+            </div>
+            <nav className={s.navigation__mobile}>
+              <NavLink to="/main" className={s.link__mobile}>
+                Main
+              </NavLink>
+              <NavLink to="/planning" className={s.link__mobile}>
+                Planning
+              </NavLink>
+              <NavLink to="/award" className={s.link__mobile}>
+                Award
+              </NavLink>
+              <NavLink to="/contacts" className={s.link__mobile}>
+                Contacts
+              </NavLink>
+            </nav>
+          </div>
+        </div>
+      )}
     </div>
   );
 };

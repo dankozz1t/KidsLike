9fcca0c9648247914e0f77b26e045c44585be8ba
Alex-Taylor Danko <AlexDankoXXL@gmail.com>
2022-10-17 10:24:18 +0300

    Releases v1.7 (#74)
    * Helen/Add-Hover-For-Card
    
    * Denis/Header
    
    * Command/Global-Fix
    
    * Command/Fix-ProgressBar
    
    Co-authored-by: ANDRII HALYTSKYI <105043191+Zorotool@users.noreply.github.com>
    Co-authored-by: Olena Yatseniuk <102380564+OlenaYatseniuk@users.noreply.github.com>
    Co-authored-by: Shpuntoff <102685000+Shpuntoff@users.noreply.github.com>
    Co-authored-by: Denis Myrhoiazov <101744823+Myrhoiazov@users.noreply.github.com>
    Co-authored-by: ArthurKorchak <105571446+ArthurKorchak@users.noreply.github.com>
    Co-authored-by: Mike Bakuzov <100297704+Bakuzov@users.noreply.github.com>

--- src/modules/Header/components/UserMenu/UserMenu.jsx
+++ src/modules/Header/components/UserMenu/UserMenu.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import { shallowEqual, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { getUser } from 'redux/auth/auth.selector';
-import { getBalance } from 'redux/task/task.selector';
-
 import Modal from 'shared/components/Modal';
 import ModalLogout from 'shared/components/ModalLogout';
+import { ReactComponent as IconBurgerMenu } from 'assets/images/icon/icon-burgerMenu.svg';
+import { ReactComponent as CloseBurgerMenu } from 'assets/images/icon/icon-BurgerClose.svg';
+import BurgerMenu from 'shared/components/BurgerMenu/BurgerMenu';
 import VerticalBar from 'shared/components/VerticalBar/VerticalBar';
 import UserInfo from '../UserInfo';
 
@@ -15,9 +16,13 @@ import s from './UserMenu.module.scss';
 import Loader from 'shared/components/Loader';
 
 const UserMenu = () => {
-  const balance = useSelector(getBalance, shallowEqual);
-  const { email } = useSelector(getUser, shallowEqual);
+  const { email, balance } = useSelector(getUser);
   const [open, setOpen] = useState(false);
+  const [isShowNav, setIsShowNav] = useState(false);
+
+  const handleIsShowMenu = () => {
+    setIsShowNav(!isShowNav);
+  };
 
   return (
     <div className={s.subcontainer}>
@@ -28,6 +33,12 @@ const UserMenu = () => {
         ) : (
           <Loader width="30" height="20" />
         )}
+      </div>
+
+      <div className={s.burger_menu}>
+        <BurgerMenu onClick={handleIsShowMenu}>
+          <IconBurgerMenu />
+        </BurgerMenu>
       </div>
       <nav className={s.navigation}>
         <NavLink to="/main" className={s.link}>
@@ -60,12 +71,53 @@ const UserMenu = () => {
           type="button"
           onClick={() => setOpen(true)}
         >
-          <LogoAuth />
+          <LogoAuth className={s.logo__auth} />
         </button>
         <Modal open={open} onClose={() => setOpen(false)}>
           <ModalLogout onClose={() => setOpen(false)} />
         </Modal>
       </div>
+      {isShowNav && (
+        <div className={s.modal_nav}>
+          <div className={s.modal_nav_wrapper}>
+            <div className={s.burger_menu}>
+              <div className={s.box__mobile}>
+                <UserInfo email={email.slice(0, 5)} />
+                <VerticalBar classFor="Header" />
+                <button
+                  className={s.btnlogout__modal}
+                  type="button"
+                  onClick={() => setOpen(true)}
+                >
+                  <LogoAuth className={s.icon__logout} />
+                </button>
+                <Modal open={open} onClose={() => setOpen(false)}>
+                  <ModalLogout onClose={() => setOpen(false)} />
+                </Modal>
+              </div>
+              <div className={s.burger_menu_close}>
+                <BurgerMenu onClick={handleIsShowMenu}>
+                  <CloseBurgerMenu />
+                </BurgerMenu>
+              </div>
+            </div>
+            <nav className={s.navigation__mobile}>
+              <NavLink to="/main" className={s.link__mobile}>
+                Main
+              </NavLink>
+              <NavLink to="/planning" className={s.link__mobile}>
+                Planning
+              </NavLink>
+              <NavLink to="/award" className={s.link__mobile}>
+                Award
+              </NavLink>
+              <NavLink to="/contacts" className={s.link__mobile}>
+                Contacts
+              </NavLink>
+            </nav>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
